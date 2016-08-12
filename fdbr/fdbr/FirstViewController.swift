591a3297cb701f525d67b2bf5c38abcc44330b91
setiady <setiady.wiguna@gmail.com>
2016-08-12 13:50:14 +0700

    create project & setup pods

---
+++ fdbr/fdbr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  fdbr
+//
+//  Created by Setiady Wiguna on 8/12/16.
+//  Copyright © 2016 Jiggie. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
