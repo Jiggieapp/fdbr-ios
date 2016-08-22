d2ec87e5591083b21d686c7d00cf4dcaba36d2ae
uuds <nuruddin@jiggieapp.com>
2016-08-22 14:55:56 +0700

    - Add Constant. - Change organization name to Female Daily.

---
+++ fdbr/fdbr/Resources/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  fdbr
+//
+//  Created by Mohammad Nuruddin Effendi on 8/22/16.
+//  Copyright © 2016 Jiggie. All rights reserved.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    
+    static let Token = DefaultsKey<NSData?>("Token")
+}
