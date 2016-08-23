d5f224755623328da2cfbda648304713cd6ddbc6
uuds <nuruddin@jiggieapp.com>
2016-08-23 14:47:14 +0700

    Add Failure case on APIResult.

--- fdbr/fdbr/Resources/APIHelper.swift
+++ fdbr/fdbr/Resources/APIHelper.swift
@@ -11,13 +11,15 @@ import Foundation
 /**
  Used to represent whether a request was successful or encountered an error.
  
- - Success: The request and all post processing operations were successful resulting in
+ - Success  : The request and all post processing operations were successful resulting in
  the native model serialization of the provided associated value.
- - Error  : The request encountered an error due to an error on the server,
+ - Failure  : The request encountered a failure due to invalid data or call conditions.
+ - Error    : The request encountered an error due to an error on the server,
  fail when serialize the model, or unknown error.
  */
 public enum APIResult<Value> {
     case Success(Value)
+    case Failure(String)
     case Error(NSError)
 }
 
