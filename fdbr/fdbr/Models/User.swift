1d6c3040aa706849f97471718b9e13e7598ccd37
uuds <nuruddin@jiggieapp.com>
2016-08-23 13:54:31 +0700

    Fix user model.

--- fdbr/fdbr/Models/User.swift
+++ fdbr/fdbr/Models/User.swift
@@ -11,9 +11,12 @@ import Mantle
 class User: MTLModel, MTLJSONSerializing {
     
     private(set) var id = 0
+    private(set) var username = ""
     private(set) var fullname = ""
     private(set) var email = ""
     private(set) var gender = ""
+    private(set) var about = ""
+    private(set) var bio = ""
     
     private(set) var hairType = 0
     private(set) var hairTexture = 0
@@ -21,17 +24,37 @@ class User: MTLModel, MTLJSONSerializing {
     private(set) var skinUndertone = 0
     private(set) var skinTone = 0
     
-    private(set) var hairConcerns = [0]
-    private(set) var skinConcerns = [0]
-    private(set) var bodyConcerns = [0]
+    private(set) var hairConcerns = [AnyObject]()
+    private(set) var skinConcerns = [AnyObject]()
+    private(set) var bodyConcerns = [AnyObject]()
     
     private(set) var numberOfReviews = 0
     private(set) var numberOfFollowers = 0
     private(set) var numberOfFollowing = 0
+    
+    private(set) var totalPoints = 0
 
     
     static func JSONKeyPathsByPropertyKey() -> [NSObject : AnyObject]! {
-        return ["" : ""]
+        return ["id" : "user_id",
+                "username" : "username",
+                "fullname" : "fullname",
+                "email" : "email",
+                "gender" : "user_gender",
+                "about" : "user_about",
+                "bio" : "user_bio",
+                "hairType" : "user_haityp_id",
+                "hairTexture" : "user_haitex_id",
+                "skinType" : "skintype_id",
+                "skinUndertone" : "skinun_id",
+                "skinTone" : "skintone_id",
+                "hairConcerns" : "user_haicon_id",
+                "skinConcerns" : "user_skicon_id",
+                "bodyConcerns" : "user_bodcon_id",
+                "numberOfReviews" : "reviewnum",
+                "numberOfFollowers" : "user_count_follower",
+                "numberOfFollowing" : "user_count_following",
+                "totalPoints" : "user_total_point"]
     }
     
 }
